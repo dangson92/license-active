ee18e1a62256c974dccc71d175cf297c203b0ce2
Son Dang <dangson.1011@gmail.com>
2026-03-26 16:45:18 +0700

    upload

--- server/index.js
+++ server/index.js
@@ -70,7 +70,8 @@ app.options('*', cors(corsOptions))
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true, parameterLimit: 50000 }))
 app.use(helmet({
-  crossOriginResourcePolicy: false, // Don't block cross-origin requests
+  crossOriginResourcePolicy: false, // Allow cross-origin image/asset loading
+  contentSecurityPolicy: false,     // Disable CSP - handled by frontend, not API server
 }))
 app.use(morgan('combined'))
 
